1409cd09b1a343da8ab3c62dd0d76499ad4ef623
Nicolas <nicolas.knoxx@gmail.com>
2020-09-29 09:29:06 -0300

    creating an orders checkout

--- src/giggin/core.cljs
+++ src/giggin/core.cljs
@@ -2,16 +2,15 @@
   (:require [reagent.core :as r]
             [giggin.components.header :refer [header]]
             [giggin.components.gigs :refer [gigs]]
-            ;[giggin.components.orders :refer [orders]]
+            [giggin.components.orders :refer [orders]]
             [giggin.components.footer :refer [footer]]))
-
 
 (defn app
   []
   [:div.container
    [header]
    [gigs]
-   ;[orders]
+   [orders]
    [footer]])
 
 (defn ^:export main
