2e9039c8700ffe7bc670fefe21fafabf331b4a66
Nicolas <nicolas.knoxx@gmail.com>
2020-09-29 09:28:47 -0300

    using the format-price helper on gigs

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -1,15 +1,19 @@
 (ns giggin.components.gigs
-  (:require [giggin.state :as state]))
+  (:require [giggin.state :as state]
+            [giggin.helpers :refer [format-price]]))
 
 (defn gigs
   []
   [:main
-   [:div.gigs (map (fn [gig]
-                     [:div.gig {:key (:id gig)}
-                      [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
+   [:div.gigs (map (fn [{:keys [id img title price desc]}]
+                     [:div.gig {:key id}
+                      [:img.gig__artwork {:src img :alt title}]
                       [:div.gig__body
                        [:div.gig__title
-                        [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-                          [:i.icon.icon--plus]] (:title gig)]
-                       [:p.gig__price (:price gig)]
-                       [:p.gig__desc (:desc gig)]]]) (vals @state/gigs))]])
+                        [:div.btn.btn--primary.float--right.tooltip
+                         {:data-tooltip "Add to order"
+                          :on-click (fn [] (swap! state/orders update id inc))}
+                         [:i.icon.icon--plus]] title]
+                       [:p.gig__price (format-price price)]
+                       [:p.gig__desc desc]]])
+                   (vals @state/gigs))]])
